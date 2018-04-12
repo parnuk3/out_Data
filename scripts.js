f7d9cd43399dd81ada4f909ce3c55f98a3b6d8ce
parnuk3 <parnuk3@gmail.com>
2018-04-12 17:18:13 +0500

    попытки сделать генератор пдф

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+<script src="jsPDF/jspdf.js"></script>
 function ch_group_tvp() {
 
     var tvp = document.getElementById("tvp").value
@@ -222,3 +223,13 @@ function draw_diagram_out_data(a){
     document.getElementById("legend").style.display = "block";
     document.getElementById("type_data_res").innerHTML = a;
 }
+function print(){
+    var doc = new jsPDF()
+    doc.triangle(60, 100, 60, 120, 80, 110, 'FD')
+    doc.setLineWidth(1)
+    doc.setDrawColor(255, 0, 0)
+    doc.setFillColor(0, 0, 255)
+    doc.triangle(100, 100, 110, 100, 120, 130, 'FD')
+}
+
+
---
+++ test.js
@@ -0,0 +1,4 @@
+<script src="jsPDF/jspdf.js"></script>
+function print() {
+    var doc = new jsPDF(orientation="portrait", unit="cm", format='letter')
+}
